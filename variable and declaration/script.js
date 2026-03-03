6baebfd44b6bb1af8022aef66be0420a04399a97
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-03 17:39:14 +0530

    if else 2

--- variable and declaration/script.js
+++ variable and declaration/script.js
@@ -184,3 +184,13 @@ const a =12;
 // } else {
 // // runs if none are true
 // }
+
+
+let marks = 78;
+if (marks >= 90) {
+console.log("A");
+} else if (marks >= 75) {
+console.log("B");
+} else {
+console.log("C");
+}
