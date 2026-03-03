eed1b9d58e1cd6d02ae2d2be173b69ae41867af8
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-03 17:39:29 +0530

    edit 2

--- variable and declaration/script.js
+++ variable and declaration/script.js
@@ -186,11 +186,11 @@ const a =12;
 // }
 
 
-let marks = 78;
-if (marks >= 90) {
-console.log("A");
-} else if (marks >= 75) {
-console.log("B");
-} else {
-console.log("C");
-}
+// let marks = 78;
+// if (marks >= 90) {
+// console.log("A");
+// } else if (marks >= 75) {
+// console.log("B");
+// } else {
+// console.log("C");
+// }
