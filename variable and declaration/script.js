dd0980aa801696a703bf33c86f6cd9a0f8d979bd
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-05 13:09:01 +0530

    edit 1

--- variable and declaration/script.js
+++ variable and declaration/script.js
@@ -240,7 +240,7 @@ const a =12;
 //  i++;
 // } while (i < 5);
 
-for (let i = 1; i <= 5; i++) {
- if (i === 3) continue;
- console.log(i); // Skips 3
-}
+// for (let i = 1; i <= 5; i++) {
+//  if (i === 3) continue;
+//  console.log(i); // Skips 3
+// }
