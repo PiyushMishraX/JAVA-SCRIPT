0354989865590ae2ab971213ab5cc5483e99ea7b
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-05 13:12:21 +0530

    for-in

--- variable and declaration/script.js
+++ variable and declaration/script.js
@@ -255,7 +255,7 @@ const a =12;
 //  console.log(num);
 // });
 
-let user = { name: "Harsh", age: 26 };
-for (let key in user) {
- console.log(key, user[key]);
-}
+// let user = { name: "Harsh", age: 26 };
+// for (let key in user) {
+//  console.log(key, user[key]);
+// }
