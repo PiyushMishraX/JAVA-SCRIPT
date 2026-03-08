a69019f537799055ff85bd91afda0dd65f32b8cd
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-08 08:36:38 +0530

    Modifiers (Change original array)

--- variable and declaration/script.js
+++ variable and declaration/script.js
@@ -355,3 +355,13 @@ let fruits = ["apple", "banana", "mango"];
 let marks = [90, 85, 78];
 console.log(marks[1]); // 85
 marks[2] = 80; // Update index 2
+
+
+// Modifiers (Change original array)
+let arr = [1, 2, 3, 4];
+arr.push(5); // Add to end
+arr.pop(); // Remove last
+arr.shift(); // Remove first
+arr.unshift(0); // Add to start
+arr.splice(1, 2); // Remove 2 items starting at index 1
+arr.reverse(); // Reverse order
