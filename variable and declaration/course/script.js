4373d01603c3568feea527b9c7537aad31608c02
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-09 22:43:24 +0530

    destructing and spread

--- variable and declaration/course/script.js
+++ variable and declaration/course/script.js
@@ -386,3 +386,6 @@ nums.forEach(n => console.log(n));
 nums.find(n => n > 2); // First match
 nums.some(n => n > 5); // At least one true
 nums.every(n => n > 0); // All true
+
+let [first, second] = ["a", "b", "c"];
+let newArr = [...nums, 99]; // Spread to copy & add
