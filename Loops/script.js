a544006087b7f6fde9613dc9b54d98929c3ab1ae
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 19:44:24 +0530

    for loop

--- Loops/script.js
+++ Loops/script.js
@@ -2,4 +2,42 @@
  // for while do-while forin forof foreach
  // imp - for while foreach ( majority scenario solve)
 
- 
+
+// 1 1 1 1 1 1 1 1
+// 1 2 3 4 5 6 7 8
+
+// kaha se jana hai -> kaha tak jana hai -> kaise jaana hai
+// for
+
+// kaha tak jana hai -> kab rukna hai -> kaise jaana hai
+//while
+
+// 1 -40 for
+// 20 - 30 for
+// hello aa jaye - while
+
+
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+
+// for(start; end; change ){
+
+// }
+
+// 1 - 100
+// for(let i = 1; i<101; i++){
+//     console.log(i);
+//     // console.log("i");
+//     // console.log("huihui");
+// }
+
+
+
+
