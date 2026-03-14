3b20489e9c82480a284dc423892a7b5f04a792eb
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 14:18:36 +0530

    comparison operators

--- Operators/script.js
+++ Operators/script.js
@@ -20,4 +20,33 @@
 
 
 
+// comparaison
+// =
+// ==  not strict
+// ===  strict
+// !=   not strict
+// !==  strict
+// >=
+// <=
+// <
+// ></>
+
+// = assignment operator, value dalana
+// 12 == 13 , //false
+// 12 == "12" // true
+// == not check type only value
+
+// 12 === "12" // false - check type also
+
+// 12 != 13 // true
+
+//  12 != "12" // flase
+// 12 !== "12" // true
+
+
+// 12 > 13 , 22<22, 22<=22 , 13>=12
+
+
+
+
 
