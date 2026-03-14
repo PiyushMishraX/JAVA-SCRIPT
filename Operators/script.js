92456e18e9ba979b946ae507209375c67131b3d1
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 14:12:28 +0530

    arithmetic operators

--- Operators/script.js
+++ Operators/script.js
@@ -7,3 +7,17 @@
 // ?:
 
 
+//Arithmatic
+// + - * / % **
+
+// 1+2 = 3
+// "ha" + "rsh" = "harsh"
+
+// 12 -2, 12*2 , 12/6
+
+// modulus 12%2 = 0 , 12%5 = 2
+// power 2**3 = 8 exponentiation
+
+
+
+
