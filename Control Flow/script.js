fc7c3610b262042c9102f1dbb46bb44e92187d36
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 16:19:41 +0530

    frade score function

--- Control Flow/script.js
+++ Control Flow/script.js
@@ -50,10 +50,78 @@
 // }
 // console.log(getVal(55)); problem always returns A
 
-function getVal(val){
+// function getVal(val){
 //     if(val<25) return 'D';
 //     else if(val<50) return 'C';
 //     else if(val <75) return 'B';
 //     else return 'A';
 // }
 // console.log(getVal(12));
+
+
+// confusing topics ---
+
+
+// function getGrade(score){
+//     if(score >= 90 && score <=100){
+//         return "A";
+
+//     } 
+//     else if(score >= 80 && score <=89)
+//     {
+//         return "B";
+//     }
+//     else if(score >= 70 && score <=79)
+//     {
+//         return "C";
+//     }
+//     else if(score >= 60 && score <=69)
+//     {
+//         return "D";
+//     }
+//     else if(score >= 33 && score <=59)
+//     {
+//         return "E";
+//     }
+//    else if(score >= 0 && score <=32)
+//     {
+//         return "Fail";
+//     }
+//     else{
+//         return "invalid marks";
+//     }
+// }
+
+// console.log(getGrade(75));
+
+
+
+
+// function getGrade(score){
+//     if(score >= 90 && score <=100) return "A";
+//     else if(score >= 80 && score <=89) return "B";
+//     else if(score >= 70 && score <=79) return "C";
+//     else if(score >= 60 && score <=69) return "D";
+//     else if(score >= 33 && score <=59) return "E";
+//     else if(score >= 0 && score <=32) return "Fail";
+//     else return "invalid marks";
+// }
+// console.log(getGrade(75));
+
+
+// function getGrade(score){
+//     if(score >= 90 && score <=100) return "A";
+//     else if(score >= 80 && score <90) return "B";
+//     else if(score >= 70 && score <80) return "C";
+//     else if(score >= 60 && score <70) return "D";
+//     else if(score >= 33 && score <60) return "E";
+//     else if(score >= 0 && score <33) return "Fail";
+//     else return "invalid marks";
+// }
+// console.log(getGrade(75));
+
+
+
+
+
+
