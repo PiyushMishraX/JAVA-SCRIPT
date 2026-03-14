af140e1e078c7c8f35820a504c108d88470ebd33
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 15:48:29 +0530

    questions

--- Operators/script.js
+++ Operators/script.js
@@ -145,55 +145,117 @@
 
 
 
-// confusions
-
+// and questions
 
 // let x = 10;
 // let y = 20;
 
 // if(x >5 && y , 25 ){
-//     console.log("A");
-// } else {
-//     console.log("B");
-// }
+    //     console.log("A");
+    // } else {
+        //     console.log("B");
+        // }
+        
+        // let isAdmin = true;
+        // let isLoggedIn = false;
+        
+        // if(isAdmin || isLoggedIn){
+            //     console.log("Access Granted");
+            // } else {
+                //     console.log("Access denied");
+                // }
+                
+                
+                // let temp =35;
+                // if(!(temp<30)){
+                    //     console.log("Hot");
+                    // } else{
+                        //     console.log("Pleasant");
+                        // }
+                        
+                        
+                        // let a = 0;
+                        // if(a){
+                            //     console.log("Truthy");
+                            // } else{
+                                //     console.log("Falsy");
+                                // }
+                                
+                                
+                                
+                                // ----- VERY VERY IMP ------
+                                // let score = 78;
+                                
+                                // let grade = score >= 90 ? "A" : score >= 75 ? "B" : score>= 60 ? "C" : Fail;
+                                
+                                // console.log(grade);
+                                
+                                
+                                // let points = 120;
+                                // let status = points > 100 ? "Gold" : points > 50 ? "Silver" : "Bronze";
 
-// let isAdmin = true;
-// let isLoggedIn = false;
+                                // console.log(status)
+                                
+                                
+                                
+                                // let loggedIn = true;
+                                // let hasToken = false;
+                                
+                                // let access = loggedIn && hasToken ? "Allow" : "Deny";
+                                
+                                // console.log(access);
+                                
+                                
+                                
+                                
+                                
+                                // incremetn decrement
+                                
+                                
+                                //  let a =5;
+                                // console.log(a++);
+                                // console.log(a);
+                                
+                                
+                                //  let b =7;
+                                // console.log(++b);
+                                // console.log(b);
+                                
+                                
+                                // let x =3;
+                                // let y = x++;
+                                // console.log(x,y);
+                                
+                                // let p =4;
+                                // let q = p++;
+                                // console.log(p,q);
+                                
+                                // let m =10;
+                                // console.log(m--);
+// console.log(m);
 
-// if(isAdmin || isLoggedIn){
-//     console.log("Access Granted");
-// } else {
-//     console.log("Access denied");
-// }
+// let n = 5;
+// let result = n++ + ++n;
+// console.log(result);
 
 
-// let temp =35;
-// if(!(temp<30)){
-//     console.log("Hot");
-// } else{
-//     console.log("Pleasant");
-// }
-
-
-// let a = 0;
-// if(a){
-//     console.log("Truthy");
-// } else{
-//     console.log("Falsy");
-// }
-
-
-
-// ----- VERY VERY IMP ------
-// let score = 78;
-
-// let grade = score >= 90 ? "A" : score >= 75 ? "B" : score>= 60 ? "C" : Fail;
-
-// console.log(grade);
-
-
-// let points = 120;
-// let status = points > 100 ? "Gold" : points > 50 ? "Silver" : "Bronze";
-
-// console.log(status)
-
+// let likes = 100;
+// function likePost() {
+    //     return ++likes;
+    // }
+    
+    // console.log(likepost());
+    // console.log(likes);
+    
+    // let count = 5;
+    // if (count-- ===  5){
+        //     console.log("Matched");
+        // } else {
+            //     console.log("Not matched");
+            // }
+            
+            
+            
+            
+            
+            
