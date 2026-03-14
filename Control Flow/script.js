9e74965c9ca4c381455b1d5db131350e3a4e1f8b
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 16:30:53 +0530

    rock paper scissor logic

--- Control Flow/script.js
+++ Control Flow/script.js
@@ -52,8 +52,8 @@
 
 // function getVal(val){
 //     if(val<25) return 'D';
-//     else if(val<50) return 'C';
-//     else if(val <75) return 'B';
+//     if(val<50) return 'C';
+//     if(val <75) return 'B';
 //     else return 'A';
 // }
 // console.log(getVal(12));
@@ -99,29 +99,59 @@
 
 // function getGrade(score){
 //     if(score >= 90 && score <=100) return "A";
-//     else if(score >= 80 && score <=89) return "B";
-//     else if(score >= 70 && score <=79) return "C";
-//     else if(score >= 60 && score <=69) return "D";
-//     else if(score >= 33 && score <=59) return "E";
-//     else if(score >= 0 && score <=32) return "Fail";
-//     else return "invalid marks";
+//     if(score >= 80 && score <=89) return "B";
+//     if(score >= 70 && score <=79) return "C";
+//     if(score >= 60 && score <=69) return "D";
+//     if(score >= 33 && score <=59) return "E";
+//     if(score >= 0 && score <=32) return "Fail";
+//     return "invalid marks";
 // }
 // console.log(getGrade(75));
 
 
 // function getGrade(score){
 //     if(score >= 90 && score <=100) return "A";
-//     else if(score >= 80 && score <90) return "B";
-//     else if(score >= 70 && score <80) return "C";
-//     else if(score >= 60 && score <70) return "D";
-//     else if(score >= 33 && score <60) return "E";
-//     else if(score >= 0 && score <33) return "Fail";
-//     else return "invalid marks";
+//     if(score >= 80 && score <90) return "B";
+//     if(score >= 70 && score <80) return "C";
+//     if(score >= 60 && score <70) return "D";
+//     if(score >= 33 && score <60) return "E";
+//     if(score >= 0 && score <33) return "Fail";
+//     return "invalid marks";
 // }
 // console.log(getGrade(75));
 
 
 
 
+//  ROCK-PAPER-SCISSORS LOGIC
+
+// function rps(user,computer){
+//     if(user == computer ) return "draw";
+
+//     if(user === "rock" && computer === "scissor") return "user";
+//     if(user === "scissor" && computer === "rock") return "computer";
+    
+//     if(user === "paper" && computer === "rock") return "user";
+//     if(user === "rock" && computer === "paper") return "computer";
+    
+//     if(user === "scissor" && computer === "paper") return "user";
+//     if(user === "paper" && computer === "scissor") return "computer";
+// }
 
 
+// console.log(rps("rock", "scissor"));
+
+
+
+
+// function rps(user,computer){
+//     if(user == computer ) return "draw";
+
+//     if(user === "rock" && computer === "scissor") return "user";
+//     if(user === "paper" && computer === "rock") return "user";
+//     if(user === "scissor" && computer === "paper") return "user";
+//     return "computer";
+// }
+
+
+// console.log(rps("rock", "scissor"));
