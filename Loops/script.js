3594fedeec7bf374aef217b4eb909b2e558a451e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 20:25:08 +0530

    loops questions

--- Loops/script.js
+++ Loops/script.js
@@ -39,5 +39,109 @@
 // }
 
 
+// start
+// while ( end){
+
+//     chahnge;
+// }
 
 
+// let i =1;
+// while(i<32){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// do{
+
+// }
+// while(end)
+
+// i = 12;
+// do {
+//     console.log(i);
+//     i++;
+    
+// } while (i<2)
+
+
+
+
+// for(let i =1 ; i <201 ; i++){
+//     console.log(i);
+//     if(i===32){
+//         break;
+//     }
+// }
+
+
+// for(let i =1 ; i <201 ; i++){
+//     if(i===32){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
+
+// Practice -- 
+
+// // Q1
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+
+// // q2
+// for (let i =10; i>0; i--){
+//     console.log(i);
+// }
+
+// let i=10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// let i =0;
+// while (i < 10){
+//     console.log(10 - i);
+//     i++;
+// }
+
+
+
+
+//q3
+// for (let i = 1; i <= 1; i++) {
+//     console.log(i * 2); // Prints 2, 4, 6
+// }
+
+
+// for(let i =1; i <=10; i++){
+//     console.log(i*2);
+//     // i++;   // increment is in loop 
+// }
+
+// for(let i =1; i <=20; i++){
+//     if(i%2 === 0)
+//     console.log(i);
+// }
+
+
+//q4
+
+// for(let i =1; i <=10; i++){
+//     console.log(i*2 - 1);
+// }
+
+// for(let i =1; i <=20; i++){
+//     if(i%2 !== 0)
+//     console.log(i);
+// }
