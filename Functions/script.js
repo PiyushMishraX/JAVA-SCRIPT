0f04abcc36254dd154891a58505395a297b6b4da
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 22:11:20 +0530

    functions

---
+++ Functions/script.js
@@ -0,0 +1,28 @@
+// what  why how
+// what function
+
+// agar code likh diyta to wo turant chal jaata hai
+
+// console.log("hey");
+// //  
+
+
+// console.log("dance");
+// console.log("dance");
+// console.log("dance");
+// console.log("dance");
+// console.log("dance");
+// console.log("dance");
+
+// function dance() {
+//     console.log("dance");
+// console.log("dance");
+// console.log("dance");
+// console.log("dance");
+// console.log("dance");
+// console.log("dance");
+// }
+
+// dance();
+// dance();
+// dance();
