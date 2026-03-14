1e3623df6a543d4eea8fae61b6ecb6048ed0785d
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 15:02:51 +0530

    clear normal confusion

--- Operators/script.js
+++ Operators/script.js
@@ -143,3 +143,47 @@
 // [] {} ()
 // typeof works with primitve values better used with them mostly  
 
+
+
+// confusions
+
+
+// let x = 10;
+// let y = 20;
+
+// if(x >5 && y , 25 ){
+//     console.log("A");
+// } else {
+//     console.log("B");
+// }
+
+// let isAdmin = true;
+// let isLoggedIn = false;
+
+// if(isAdmin || isLoggedIn){
+//     console.log("Access Granted");
+// } else {
+//     console.log("Access denied");
+// }
+
+
+// let temp =35;
+// if(!(temp<30)){
+//     console.log("Hot");
+// } else{
+//     console.log("Pleasant");
+// }
+
+
+// let a = 0;
+// if(a){
+//     console.log("Truthy");
+// } else{
+//     console.log("Falsy");
+// }
+
+
+
+
+
+
