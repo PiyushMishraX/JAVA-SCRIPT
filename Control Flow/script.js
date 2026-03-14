e848714aadfec7b3da9f0a93408efacdffafe0f2
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-14 16:01:14 +0530

    control flow - if else and switch case

---
+++ Control Flow/script.js
@@ -0,0 +1,38 @@
+// if else else-if
+// switch case
+// early return pattern
+
+
+// if(condition -> true or false value)
+
+// if(65){
+
+// } else{
+
+// }
+
+
+// if(loggedin && admin){
+
+// } else if(loggedin){
+
+// }else {
+
+// }
+
+// can have infinite else if blocks
+
+
+// SWITCH CASE
+
+// switch(1){
+//     case 1:
+//         console.log("bruh");
+//         // break;
+//     case 2:
+//         break;
+//     case 3:
+//         console.log("hey");
+//         break;
+//     default:
+// }
