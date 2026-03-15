a785eea7c47a1d0faa92db0834377b62b6e6b7ec
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-15 21:58:50 +0530

    objects featuers js

--- Objects/script.js
+++ Objects/script.js
@@ -76,4 +76,103 @@
 
 
 
-// 
+// objct keys
+
+// let obj = {
+//     name: "harsh",
+//     age: 26,
+//     email: "test@test.com",
+// };
+
+// Object.keys(obj);
+// arr of keys of object
+
+// Object.entries(obj);
+// (3) [Array(2), Array(2), Array(2)]0: (2) ['name', 'harsh']1: (2) ['age', 26]2: (2) ['email', 'test@test.com']length: 3[[Prototype]]: Array(0)
+// array of arrays
+
+
+// copying object
+
+// let obj = {
+//     name: "harsh",
+//     age: 26,
+//     email: "test@test.com",
+// };
+
+// let obj2 = { ...obj}; // spread
+
+// object .assign -> mostly spread is used always it is nearly not used
+
+// let obj2 = Object.assign({} , obj);
+
+// let obj2 = Object.assign({price:Infinity} , obj);
+
+
+
+// deep cloning
+
+// let obj = {
+//     name: "harsh",
+//     age: 26,
+//     email: "test@test.com",
+//     address: {
+//         city: "bhopal",
+//     },
+// };
+
+// let obj2 = { ...obj};
+// obj2.address.city = "Indore"; 
+// // onj.city -> indore too 
+// // in nested objects only top level values only copied for real
+// // the nested objects pass refernces again
+
+// solution deep clone 
+
+
+// JSON.stringify(obj) // -> obj totally changes to string
+// '{"name":"harsh","age":26,"email":"test@test.com","address":{"city":"bhopal"}}'
+// JSON.parse('{"name":"harsh","age":26,"email":"test@test.com","address":{"city":"bhopal"}}')
+// {name: 'harsh', age: 26, email: 'test@test.com', address: {…}}
+
+// then untring it , the real copy is made
+
+
+// deep cloning
+// let obj2 = JSON.parse(JSON.stringify(obj));
+//  obj2.address.city = "Indore";
+
+
+// Optional chaining , computed properties
+
+// //  Optional chaining
+// let obj = {
+//     name: "harsh",
+//     age: 26,
+//     email: "test@test.com",
+//     addresses: {
+//     // address: {
+//         city: "bhopal",
+//     },
+// };
+
+// // obj.address.city;
+// obj?.address?.city; // not give error -> gives undefined
+
+
+
+// Computed properties
+
+let role = "admin";
+
+let obj = {
+    name: "harsh",
+    age: 26,
+    email: "test@test.com",
+    addresses: {
+    // address: {
+        city: "bhopal",
+    },
+    [role]: "harsh",
+    // admin: "harsh";
+};
