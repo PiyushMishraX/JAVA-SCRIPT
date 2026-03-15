a6a0860c5b7b7c1ca2a7da5e7fc47228f72c42bc
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-15 19:33:03 +0530

    array

---
+++ Arrays/script.js
@@ -0,0 +1,15 @@
+// let arr = [1,2,3,4];
+// let marks = [ 10, 40, 52, 37 , 92];
+// // 0th  index
+// index(STARTs from 0 to n -1) = poition( startig from 1 to n) - 1
+
+// // marks[0] = 10
+// // marks[3] = 37;
+// // marks[50] = undefined
+
+// let arr = new Array();
+
+
+// let arr = [1,2,3,4];
+// arr[2] = 12;
+// console.log(arr);
