c34fc20c23ed3548de20097898f2f54e79cc848e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-16 11:58:01 +0530

    attribute manipulation

--- DOM (Documetn Object Model )/script.js
+++ DOM (Documetn Object Model )/script.js
@@ -44,27 +44,66 @@
 // --- TEXT MANIPULATION
 // hey hey kaise ho change
 
-let h1 = document.querySelector("h1");
+// let h1 = document.querySelector("h1");
 
-console.dir(h1);
+// console.dir(h1);
 
-// changing a value with text
+// // changing a value with text
 
-// propetries such as TextContent , inner Html , innerText , OuterText etc - change them 
-// check props using console.dir and change them 
-// the [properties are in form of key- value pairs like object
+// // propetries such as TextContent , inner Html , innerText , OuterText etc - change them 
+// // check props using console.dir and change them 
+// // the [properties are in form of key- value pairs like object
 
-// h1.textContent = "hello piyush kaise ho";
-// h1.innerHTML = " Piyush is good";
-// h1.innerText = "so good";
-// h1.outerText = "so good";
-// h1.TextContents = " bruh"; XX
+// // h1.textContent = "hello piyush kaise ho";
+// // h1.innerHTML = " Piyush is good";
+// // h1.innerText = "so good";
+// // h1.outerText = "so good";
+// // h1.TextContents = " bruh"; XX
 
-// innerText change text , TextContent same as innerText , innerHTML change html
+// // innerText change text , TextContent same as innerText , innerHTML change html
 
-h1.innerHTML = "<i>hey </i>";
-// h1.innerText = "<i>hey </i>";
+// h1.innerHTML = "<i>hey </i>";
+// // h1.innerText = "<i>hey </i>";
 
-h1.hidden = true;
+// h1.hidden = true;
  
-console.dir(h1);
+// console.dir(h1);
+
+
+
+
+// ---  Attribute Manipulation --- 
+
+// let a = document.querySelector("a");
+// console.dir(a);
+// // a.href = "https://google.com";
+
+// a.setAttribute("href","https://google.com"
+// );
+// console.dir(a);
+
+//  document.getElementById("result").textContent = "The href attribute value is: " + hrefValue;
+
+
+
+// let img = document.querySelector("img");
+// img.setAttribute(
+//         "src",
+//         "https://images.unsplash.com/photo-1773539649767-98dc585833fa?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+//     );
+    
+//     let srcval = img.getAttribute('src');
+//     console.log(srcval);
+
+
+
+//getAttribute
+
+// let a = document.querySelector("a");
+// console.log(a.getAttribute("href"));
+
+// remove attribute
+// let a = document.querySelector("a");
+// a.removeAttribute("href");
+// console.log(a.getAttribute("href"));
+
