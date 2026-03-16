5fa3e52c7036cd16a58926c424e71a0520ef9f5a
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-16 11:27:34 +0530

    text manipulation

--- DOM (Documetn Object Model )/script.js
+++ DOM (Documetn Object Model )/script.js
@@ -41,3 +41,30 @@
 
 
 
+// --- TEXT MANIPULATION
+// hey hey kaise ho change
+
+let h1 = document.querySelector("h1");
+
+console.dir(h1);
+
+// changing a value with text
+
+// propetries such as TextContent , inner Html , innerText , OuterText etc - change them 
+// check props using console.dir and change them 
+// the [properties are in form of key- value pairs like object
+
+// h1.textContent = "hello piyush kaise ho";
+// h1.innerHTML = " Piyush is good";
+// h1.innerText = "so good";
+// h1.outerText = "so good";
+// h1.TextContents = " bruh"; XX
+
+// innerText change text , TextContent same as innerText , innerHTML change html
+
+h1.innerHTML = "<i>hey </i>";
+// h1.innerText = "<i>hey </i>";
+
+h1.hidden = true;
+ 
+console.dir(h1);
