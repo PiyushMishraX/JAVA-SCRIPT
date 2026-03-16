b3e5cbbb99a6151561864fe1ffd961ccfebdf7fe
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-16 11:13:04 +0530

    DOM / DOM manipulation

---
+++ DOM (Documetn Object Model )/script.js
@@ -0,0 +1,43 @@
+// for now - DOM =body 
+// everything inside body is  dom 
+// DOM manipulation
+
+
+// dom manulation needs to learn
+// selcting html elemts
+// changing text
+// / changing html
+// changing CSS
+// changing attribute
+// adding event listener
+
+// and full of common sense and use og mind
+
+
+
+/// ----  SELECTION -----
+
+// document.querySelector
+// document.getElementById
+// document.getElementsByClassName
+
+// let abcd = document.getElementById("abcd");
+// console.dir(abcd); // opening format
+// console.log(abcd); // sometimes opening format aometimes tag
+
+// let ghij = document.getElementsByClassName("ghij");
+// console.dir(ghij); 
+
+// these are old methods
+
+// new and standartd method/s -
+
+// let v1 =document.querySelector("h1"); // select first
+// console.dir(v1);
+
+
+// let v1 =document.querySelectorAll("h1"); // select all
+// console.dir(v1); // looks like array but is node list
+
+
+
