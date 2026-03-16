40069f45b060cd6e6885848e60caa32b5db8fd99
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-16 17:01:16 +0530

    task 1

---
+++ DOM (Documetn Object Model )/tasks.js
@@ -0,0 +1,10 @@
+// task 1
+// slect the heading of a page by ID and change its text to "Welcome to sheriyans! ".
+
+
+// let page_title = document.querySelector("#page-title");
+// page_title.textCOntent = "Welcome to sheriyans! ";
+
+
+// let head = document.querySelector("#heading");
+// head.textContent = "Welcome to sheriyans !";
