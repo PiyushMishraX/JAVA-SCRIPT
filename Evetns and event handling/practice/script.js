3516b13a06d0c4cb1c05261f5638f8c497699832
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-17 16:44:35 +0530

    todo list

--- Evetns and event handling/practice/script.js
+++ Evetns and event handling/practice/script.js
@@ -20,3 +20,29 @@
 //         span.style.color = "white";
 //     }
 // });
+
+
+// deligated todo lst already created line-through in taks
+
+let form = document.querySelector("form");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let li = document.createElement("li");
+    li.textContent = inp.value;
+    ul.appendChild(li);
+    inp.value ='';
+    console.dir(ul);
+});
+
+ul.addEventListener("click", function(dets){
+    dets.target.classList.toggle("lt");
+    // console.log(dets);
+    console.dir(dets);
+    if(this.children.length >= 5){
+        // ul.remove(dets.target);
+        dets.target.remove();
+    }
+});
