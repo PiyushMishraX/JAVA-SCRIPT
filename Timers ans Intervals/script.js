46bfd8ade6d01dcb41640ba69589842434cdab8d
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-17 19:55:47 +0530

    set timeout and set interval

---
+++ Timers ans Intervals/script.js
@@ -0,0 +1,12 @@
+// console.log("hello");
+
+// after time
+// setTimeout(() => {
+//   console.log("hello");
+// }, 5000); // 5000 = 5s or 5000 milli second
+
+// setInterval(()=>{
+//    console.log("kutti");   
+// },1000);
+
+
