f9d2b2fb3673fc791e6e3d448c8aa5c50be6dfd3
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-17 18:05:10 +0530

    pattern attribut

--- Forms and fFOrm validation/script.js
+++ Forms and fFOrm validation/script.js
@@ -13,3 +13,19 @@
 //     }
 // });
 
+// showing error meassages occasionally is n=the bove 
+
+
+// pattern attribute
+// rearely used
+// in html attribute
+//  pattern="[a-z]{3,8}"
+
+
+// regex - it is a different language - is it code of patterns use chat gpt for wring it in start
+
+// regex
+
+form.addEventListener("submit" , function(dets){
+   
+});
