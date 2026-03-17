7fc9e97e88ea2f3196923ff622acd53c680e182e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-17 21:17:23 +0530

    sessiion storage

--- localStorage, sessionStorage and  Cookies/script.js
+++ localStorage, sessionStorage and  Cookies/script.js
@@ -21,3 +21,17 @@
 
 // Update item - set item hi updata karta hai
 // localStorage.setItem("name" , "piyush mishra");
+
+localStorage.clear();
+//
+
+
+
+// setssion storage -> deletes when tab closed or new tab opened  in console, unlike local storage which works even after tab is closed and reopened ( but not after browser closed or don't know)
+// the saves propeertiedso flocal storage transcend to other tabs but of session storage doesn't 
+
+// sessionStorage.setItem("name", "piyush"); // write in console not here to test them 
+
+// let v1 = sessionStorage.getItem("name");
+// sessionStorage.removeItem("name");
+// sessionStorage.setItem("name", "piyush mishra");
