8c10b4662d68b2c1b2cfa0cc05741eebe96b626e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-17 17:52:47 +0530

    inline validation

--- Forms and fFOrm validation/script.js
+++ Forms and fFOrm validation/script.js
@@ -1,13 +1,15 @@
-let nm = document.querySelector("#name");
-let form = document.querySelector("form");
+// Js validation ( small )
+// let nm = document.querySelector("#name");
+// let form = document.querySelector("form");
 
 
-form.addEventListener("submit" , function(dets){
-    dets.preventDefault();
+// form.addEventListener("submit" , function(dets){
+//     dets.preventDefault();
 
-    if(nm.value.length <= 2){
-        document.querySelector("#hide").style.display = "initial";
-    }else{
-        document.querySelector("#hide").style.display = "none";
-    }
-});
+//     if(nm.value.length <= 2){
+//         document.querySelector("#hide").style.display = "initial";
+//     }else{
+//         document.querySelector("#hide").style.display = "none";
+//     }
+// });
+
