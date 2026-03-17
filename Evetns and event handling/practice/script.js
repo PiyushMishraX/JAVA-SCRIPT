af5ca090e0727a21b5ac3d5aced61a6c465a9d96
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-17 16:23:08 +0530

    lve character counter

---
+++ Evetns and event handling/practice/script.js
@@ -0,0 +1,22 @@
+// // Practce 1 --- 
+
+// let inp = document.querySelector("input");
+// let span = document.querySelector("span");
+
+// inp.addEventListener("input", function(){
+//     // console.log("input hua");
+//     // console.log(inp.value.length);
+//     // span.textContent = inp.value.length;
+//     // span.textContent = 20 - inp.value.length;
+//     let left = 20 - inp.value.length;
+//     span.textContent = left;
+
+//     if(left<0){
+//         // span.textContent = left;
+//         span.style.color = "red";
+//     }
+//     else{
+//         // span.textContent = left;
+//         span.style.color = "white";
+//     }
+// });
