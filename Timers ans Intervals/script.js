553376f436c4401224368c9c441919308b529809
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-17 20:07:58 +0530

    counter

--- Timers ans Intervals/script.js
+++ Timers ans Intervals/script.js
@@ -25,14 +25,16 @@
 
 // create counter 10 to 0
 
-let count = 10;
+// let count = 10;
 
-let interval = setInterval( function(){
-    if(count >= 1 ){
-        count--;
-        console.log(count);
-    }
-    else{
-        clearInterval(interval);
-    }
-},1000);
+// let interval = setInterval( function(){
+//     if(count >= 1 ){
+//         count--;
+//         console.log(count);
+//     }
+//     else{
+//         clearInterval(interval);
+//     }
+// },1000);
+
+// counter from 9 to 0
