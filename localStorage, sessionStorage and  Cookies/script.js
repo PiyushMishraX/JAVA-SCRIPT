3fe3be093a3d74ccbb10debf25ddad601c373370
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-17 21:00:34 +0530

    local , session storage and cookies

---
+++ localStorage, sessionStorage and  Cookies/script.js
@@ -0,0 +1,6 @@
+// locaStorage -> aapke browser ke andar data store karna jo ki browser band hone par bhi delete nahi hoga 9 browser ka data base)
+// sessionStorage -> ye apka daa temporarily store karta hai matlab ki tab band hua aur data gaya 
+
+// cookies ->  ye bhi data store karta hai and aapka data browser ke cookies naam ki   property mein save hota hai  and ye cookie concept kam data ya light data ke liye hota hai
+
+
