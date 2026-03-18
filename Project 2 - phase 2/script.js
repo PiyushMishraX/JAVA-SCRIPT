e7f5f052c37f92038a267d1b923efc712437ef3e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-18 14:49:07 +0530

    submit listener

--- Project 2 - phase 2/script.js
+++ Project 2 - phase 2/script.js
@@ -3,6 +3,24 @@ let addNote = document.querySelector("#add-note");
 let formContainer = document.querySelector(".form-container");
 let closeForm = document.querySelector(".closeForm");
 
+const form = document.querySelector("form");
+
+const imageUrlInput = form.querySelector(
+  "input[placeholder='https://example.com/photo.jpg']"
+);
+const fullNameInput = form.querySelector(
+  "input[placeholder='Enter full name']"
+);
+const homeTownInput = form.querySelector(
+  "input[placeholder='Enter home town']"
+);
+const purposeInput = form.querySelector(
+  "input[placeholder='e.g., Quick appointment note']"
+);
+
+const categoryRadios = form.querySelectorAll("input[name='category']");
+
+
 
 // functionality
 
@@ -12,4 +30,22 @@ addNote.addEventListener("click", function () {
 
 closeForm.addEventListener("click", function () {
   formContainer.style.display = "none";
+});
+
+// form validation reuired in html 
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const imageUrl = imageUrlInput.value.trim();
+  const fullName = fullNameInput.value.trim();
+  const homeTown = homeTownInput.value.trim();
+  const purpose = purposeInput.value.trim();
+
+  let selected = false;
+  categoryRadios.forEach(function (cat) {
+    if (cat.checked) {
+      selected = cat.value;
+    }
+  });
+
 });
