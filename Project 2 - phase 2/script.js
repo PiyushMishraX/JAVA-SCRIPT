88287ae5f1236ae54f456671c47f9064f7fd2e75
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-18 14:46:23 +0530

    close form

--- Project 2 - phase 2/script.js
+++ Project 2 - phase 2/script.js
@@ -1,10 +1,15 @@
 // variable and docs
 let addNote = document.querySelector("#add-note");
-
+let formContainer = document.querySelector(".form-container");
+let closeForm = document.querySelector(".closeForm");
 
 
 // functionality
 
 addNote.addEventListener("click", function () {
   formContainer.style.display = "initial";
+});
+
+closeForm.addEventListener("click", function () {
+  formContainer.style.display = "none";
 });
