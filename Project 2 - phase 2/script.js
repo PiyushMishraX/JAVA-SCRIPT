fb2b049a5d24c0bc6f4391d07ba09fcdec33ff9e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-18 14:45:36 +0530

    addNOte

--- Project 2 - phase 2/script.js
+++ Project 2 - phase 2/script.js
@@ -1,4 +1,10 @@
-
-
-
+// variable and docs
 let addNote = document.querySelector("#add-note");
+
+
+
+// functionality
+
+addNote.addEventListener("click", function () {
+  formContainer.style.display = "initial";
+});
