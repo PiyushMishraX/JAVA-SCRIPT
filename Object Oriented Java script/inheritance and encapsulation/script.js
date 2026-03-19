be30a71b7df4be53d658a423a9380422700e9c15
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-19 22:12:05 +0530

    classical vs prototypal inheritance

---
+++ Object Oriented Java script/inheritance and encapsulation/script.js
@@ -0,0 +1,30 @@
+// classical inheritance - java cpp
+// js - class and extend
+// class banana and unhe extend krdena
+
+// inheritance - class -> class property lena
+
+// prototypal inheritance - object -> object ( se inherit )
+
+let coffee = {
+    color: "dark",
+    drink: function () {
+        console.log("drinked it");
+    },
+};
+
+let arabiataCoffee = Object.create(coffee);
+// Object.create passed obj ke protype ko other object ke prototype se jood deta hai 
+console.log(arabiataCoffee);
+arabiataCoffee.drink();
+
+arabiataCoffee.taste = "bitter";
+arabiataCoffee.color = "bitter"; // create an other color sepearate from the color in prototype instead of reassigning 
+console.log(arabiataCoffee);
+
+// ek object hai aap chaho to uski saari props/methods ko inherit kara dete ho doosre object mein
+
+let a = {};
+let b1 = Object.create(a); // shareed memory baani
+
+let b2 = {...a}; // hard copy baani  shared nahi so valeus ek change and take more memoty too
