3150d40327f19b9712ecc55b1cfaf1131fe7c4d2
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-19 21:07:33 +0530

    prototype and prototype function

--- Object Oriented Java script/script.js
+++ Object Oriented Java script/script.js
@@ -43,5 +43,56 @@
 // pencil1.write("kaise ho bhai") // black
 // pencil2.write("badhiya")// red
 
+// new keyword se constructor function ka instance banta hai     new CreatePencil(
 // these exaplme of contructor functions 
 // writing first letter capital is good practice
+
+// this ka jo likha hoga wo sab ke liye khud ki ban jaayegi only they own them ( if not hadcoded values)
+
+// if value hard coded then eveyones will be same
+
+
+// function CreatePencil(name, price , color ){
+//     this.name = name; 
+//     this.price = price;
+//     this.color = color;
+//     // this.company = "sheriyans";  is constructor function ein mat likho baar baar memory space lega ye
+
+//     this.write = function (text) {
+//         let h1 = document.createElement("h1");
+//         h1.textContent = text;
+//         h1.style.color = color;
+//         document.body.append(h1);
+        
+//     }
+// }
+// // create prototype for createpencil
+// // CreatePencil.prototype.company = "Sheriyans";
+
+// let pencil1 = new CreatePencil("Natraj", 10 , "black");
+// let pencil2 = new CreatePencil("doms", 10 , "red");
+
+
+// agar constructor constructor fn koi field apne prototype par attach karle to us constructor se nanne wale sabhi new instances yaani ki objects, ke paas wo fileds automatically chally jati hai
+
+// CreatePencil.prototype.company = "Sheriyans"; // shared -- baar baar memory nahi lega
+
+
+// use in write fn 
+function CreatePencil(name, price , color , company){
+    this.name = name; 
+    this.price = price;
+    this.color = color;
+    this.company = company;
+
+    
+}
+CreatePencil.prototype.write = function(text){
+        let h1 = document.createElement("h1");
+        h1.textContent = text;
+        h1.style.color = this.color; // this lagane se object ki value se aayengi
+        document.body.append(h1);
+}
+
+let pencil1 = new CreatePencil("Natraj", 10 , "black" , "natraj");
+let pencil2 = new CreatePencil("doms", 10 , "red" , "doms");
