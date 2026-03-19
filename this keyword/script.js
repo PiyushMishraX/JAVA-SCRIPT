e260cff1c677597109fa5117ec6374d2c277f35b
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-19 16:15:41 +0530

    fat arrow and this min method

--- this keyword/script.js
+++ this keyword/script.js
@@ -31,41 +31,54 @@ abcd();
 // // this = obj 
 
 
-let obj = {
-    name: "Piyush",
-      age: 20,
-      sayName: function(){
-        console.log(this);
-        // console.log(this.age);
-        // this ki value object method ke andar poore obj ko deenote karta hai
-      }    
-};
-obj.sayName();
+// let obj = {
+//     name: "Piyush",
+//       age: 20,
+//       sayName: function(){
+//         console.log(this);
+//         // console.log(this.age);
+//         // this ki value object method ke andar poore obj ko deenote karta hai
+//       }    
+// };
+// obj.sayName();
 
 
 // event handler (this)
 
-document.querySelector("h1")
-.addEventListener("click", function () {
-    // alert();
-    console.log(this);// this wahi hoga jisme event listener laga hai and this mai uski poori details aa jayengi jaise function drets mai aa jati hai
-    console.log((this.style.color = "red"));
+// document.querySelector("h1")
+// .addEventListener("click", function () {
+//     // alert();
+//     console.log(this);// this wahi hoga jisme event listener laga hai and this mai uski poori details aa jayengi jaise function drets mai aa jati hai
+//     console.log((this.style.color = "red"));
     
-});
+// });
 
 
 // in class
 
-class Abcd{
-    // first letter capital
-    constructor(){
-        console.log("heyhey");
-        this.a = 12;
-    }
-}
+// class Abcd{
+//     // first letter capital
+//     constructor(){
+//         console.log("heyhey");
+//         this.a = 12;
+//     }
+// }
 
-let val = new Abcd(); // blank object , Abcd chala , uska constructor chala aur jaha jaha this likha hai waha waha blank obj aagaya 
+// let val = new Abcd(); // blank object , Abcd chala , uska constructor chala aur jaha jaha this likha hai waha waha blank obj aagaya 
 // fir uske andar a ban gaya
 // val i s instance of Abcd
 
 // class ke andar this ki value blank obj hoti hai jab new ke ssath call karte ho
+
+
+let obj = {
+    name: "Piyush",
+    sayName: () => { 
+        // arrow se this value loose kar dega 
+        // this window ke barabar ho jayega
+         console.log(this); // window
+         console.log(this.name);   // ye blank ho jayega
+
+    },
+};
+obj.sayName();
