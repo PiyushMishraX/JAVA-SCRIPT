b89a604adee971c6ea27d5972f6ea1dc37e2fa7c
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-19 23:36:19 +0530

    asynchronous

---
+++ Asynchronus/script.js
@@ -0,0 +1,2 @@
+// koi bhi code js mei lin ebhi line chalega aur ye natural patter bhi hota hai ki code line by line chale, but kabhi kabaar aise cases aate hai real life mein jaha par aapka code wait krta hai and utni der mein agla code chal jaata hai
+
