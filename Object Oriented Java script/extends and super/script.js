ab28bdcb1b833609579bbd9e2de9cecb270404f8
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-19 22:00:04 +0530

    extend and super

--- Object Oriented Java script/extends and super/script.js
+++ Object Oriented Java script/extends and super/script.js
@@ -1,2 +1,38 @@
 // extends and super
 
+class User {
+  constructor(name, address, username, email) {
+    this.name = name;
+    this.address = address;
+    this.username = username;
+    this.email = email;
+    this.role = "User";
+  }
+  write(text) {
+    let h1 = document.createElement("h1");
+    h1.textContent = `${this.name} : ${text}`;
+    document.body.append(h1);
+  }
+  checkRole(){
+    // console.log(`you are a ${this.role}`);
+    return (`you are a ${this.role}`);
+  }
+}
+
+class Admin extends User {
+    constructor(name, address, username, email){
+        super(name, address, username, email); // parent ka constructor cchalega fir asmin ka constrcuttot ka work
+        this.role = "admin";
+    }
+    // onlyt admin have remove 
+    remove(){
+        document.querySelectorAll("h1").forEach(function(elem){
+            elem.remove();
+        })
+    }
+}
+
+let u1 = new User("Piyush", "Bhopal" , "asyn123" , "a@a.a" );
+let u2 = new User("1", "jbp" , "asyn123" , "a@a.a" );
+
+let a1 = new Admin("A1", "india", "asmin", "ad!ad.a");
