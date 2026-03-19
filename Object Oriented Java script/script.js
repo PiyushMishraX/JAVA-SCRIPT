83ec59741191ed3f510962dd7d522d6be83c2858
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-19 20:54:52 +0530

    constructor functions

--- Object Oriented Java script/script.js
+++ Object Oriented Java script/script.js
@@ -20,24 +20,28 @@
 
 // humein sikhana hai factories banana , matlab ki aap ek baar blueprint bana do ki object kaisa dikhega aur ham log naye naye object with different values bana paayenge , yahi upar upar se pura kam hai oops mein
 
-function CreatePencil(name, price , color , company){
-    this.name = name; // new laga hai fn ke andar this ki value blank obj hogi
-    this.price = price;
-    this.color = color;
-    this.company = company;
 
-    this.write = function (text) {
-        let h1 = document.createElement("h1");
-        // h1.textContent = "hey how are you ";
-        h1.textContent = text;
-        h1.style.color = color;
-        document.body.append(h1);
+// function CreatePencil(name, price , color , company){
+//     this.name = name; // new laga hai fn ke andar this ki value blank obj hogi
+//     this.price = price;
+//     this.color = color;
+//     this.company = company;
+
+//     this.write = function (text) {
+//         let h1 = document.createElement("h1");
+//         // h1.textContent = "hey how are you ";
+//         h1.textContent = text;
+//         h1.style.color = color;
+//         document.body.append(h1);
         
-    }
-}
+//     }
+// }
 
-let pencil1 = new CreatePencil("Natraj", 10 , "black" , "natraj");// new se blank obj baneg or this usi blank obj ko denote karega 
-let pencil2 = new CreatePencil("doms", 10 , "red" , "doms");
+// let pencil1 = new CreatePencil("Natraj", 10 , "black" , "natraj");// new se blank obj baneg or this usi blank obj ko denote karega 
+// let pencil2 = new CreatePencil("doms", 10 , "red" , "doms");
 
-pencil1.write("kaise ho bhai") // black
-pencil2.write("badhiya")// red
+// pencil1.write("kaise ho bhai") // black
+// pencil2.write("badhiya")// red
+
+// these exaplme of contructor functions 
+// writing first letter capital is good practice
