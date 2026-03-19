6a713d3c57d43a526512847ce616038401cb1d2c
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-19 23:40:11 +0530

    asynchronous vs synchronous

--- Asynchronus/script.js
+++ Asynchronus/script.js
@@ -1,2 +1,16 @@
 // koi bhi code js mei lin ebhi line chalega aur ye natural patter bhi hota hai ki code line by line chale, but kabhi kabaar aise cases aate hai real life mein jaha par aapka code wait krta hai and utni der mein agla code chal jaata hai
 
+// ex
+// console.log("hey1");
+// console.log("hey2");
+// setTimeout(() => {
+//   console.log("hey3");
+// }, 2000);
+// console.log("hey4");
+
+
+// sync // async 
+// sync vs async
+
+//aisa code jo line by line chale hota hai sync
+// aisa code jo jab chalne ke liye ready ho jaye tab chale wo hai async
