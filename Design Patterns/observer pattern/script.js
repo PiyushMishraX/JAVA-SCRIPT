0b2d91943d60a52858c94c38f445231505870ffd
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-20 22:09:24 +0530

    observer pattern explained

--- Design Patterns/observer pattern/script.js
+++ Design Patterns/observer pattern/script.js
@@ -5,6 +5,7 @@
 class YoutubeChannel {
     constructor(){
         this.subscribers = [];
+        // this.subscribers = [{naem: 'piyush', update}];
     }
 
     subscribe(user){
@@ -42,3 +43,16 @@ channel.subscribe(user2);
 // channel.unsubscribe(user1);
 
 channel.notify("new video is live on the channel...");
+
+channel.notify("we are closing the channel its april 1st");
+
+
+// youtubr channel - " subject "
+// user - "observer"
+
+// Jab ek object (Subject) ka state change hota hai, toh woh apne saare linked objects (Observers) ko automatically notify kar deta hai.
+
+// Seedhi baat ye hai ki "tu mujhe subscribe kar le, jab bhi naya maal aayega main tujhe khud message bhej dunga."
+
+
+// Jab ek object ki state change hoti hai, toh usse jude saare dependent objects ko automatically update mil jata hai. Ye one-to-many relationship maintain karta hai taaki saare objects hamesha synchronized rahein.
