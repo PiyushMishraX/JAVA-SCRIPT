79d7585f3a1f0f851865fb5c4ca2bc2162b7b6a5
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-20 14:12:13 +0530

    fetch

--- Fetch API + HTTP Basics/script.js
+++ Fetch API + HTTP Basics/script.js
@@ -5,4 +5,21 @@
 //     "name" : "piyush";
 // }
 
+// https://randomuser.me/api/
+// use jsonview extension to see it properly and formattted
 
+
+// Fetch API -- 
+// fetch is inbult fn in js - it goes to url and retrive data
+
+// fetch promise based hai - so two types to write code then catcch adn async await 
+
+fetch("https://randomuser.me/api/")
+.then(rawdata=> { //function (rawdata) { // can use it
+    console.log(rawdata);
+})
+.catch((err)=> {
+    console.log(err);
+});
+
+// ok true and status 200 in response means everything good
