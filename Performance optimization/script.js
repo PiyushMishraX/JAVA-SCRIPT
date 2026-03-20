7570eeac1704ec66edb0c9262a41f24c2b99c3d5
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-20 22:20:07 +0530

    debouncing

--- Performance optimization/script.js
+++ Performance optimization/script.js
@@ -5,6 +5,9 @@
 // jab koi specific event occur ho to jo  kisi specific interval ke baad chale wo de bouncing and jab koi code har kuch der mai chale to wo throttling - agar ek specific evetn chal raha hai tab tak har kuch der mai
 
 
-// deboucing - koi action kar rahe and aap ye nahi chahate har action pe kuchh h , jab bhi mere actons ke bech mein koi specific 
+// deboucing - koi action kar rahe and aap ye nahi chahate har action pe kuchh h , jab bhi mere actons ke bech mein koi specific gap aa jaye to fir action ka reaction perform ho
+
+
+
 
 
