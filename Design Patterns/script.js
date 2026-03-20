e0fad5083331071ef9f5c2517db23ae3e8c71b91
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-20 20:32:45 +0530

    module  pattern

--- Design Patterns/script.js
+++ Design Patterns/script.js
@@ -1,2 +1,12 @@
 // big level pe mantanable , scalabele abd modular code
 
+// module patter (IIFE) revealing module, factory function and observer ( basic pub sub ) pattern
+
+
+// module pattern 
+
+// module pattern ek design pattern hai jisme hum apna code ek self executing function (IIFE) ke andar likhte hain, taki variables aur functions private rahen.
+
+// iske andar se hum ssirf wahi cheesein return karte hain jo bahar use karni hain.
+
+// is pattern ka main fayda hai data hiding ( encapsultion ) aur clean structure , taaki code secure , reusable aur managebe ban sake.
