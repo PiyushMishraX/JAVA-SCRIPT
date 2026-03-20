86b17114e710d3950f15ecb257b4f45ad3dc3ad3
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-20 20:50:27 +0530

    IIFE

--- Design Patterns/script.js
+++ Design Patterns/script.js
@@ -2,11 +2,17 @@
 
 // module patter (IIFE) revealing module, factory function and observer ( basic pub sub ) pattern
 
+// module pattern
 
-// module pattern 
-
-// module pattern ek design pattern hai jisme hum apna code ek self executing function (IIFE) ke andar likhte hain, taki variables aur functions private rahen.
+// module pattern ek design ( code likhne ka taarika ) pattern hai jisme hum apna code ek self executing function (IIFE) ke andar likhte hain, taki variables aur functions private rahen.
 
 // iske andar se hum ssirf wahi cheesein return karte hain jo bahar use karni hain.
 
 // is pattern ka main fayda hai data hiding ( encapsultion ) aur clean structure , taaki code secure , reusable aur managebe ban sake.
+
+// imideatly invloke function expression -- no need to call it it runs
+// let fnc = (function () {
+//   // return 12;
+//   let bankbalance = 12000; // canot be excessed directly private variablE
+// })();
+
