7e788ebe51b26d2f7a554dc961fac0afa188ab95
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-20 14:18:02 +0530

    rawdata to json

--- Fetch API + HTTP Basics/script.js
+++ Fetch API + HTTP Basics/script.js
@@ -14,12 +14,28 @@
 
 // fetch promise based hai - so two types to write code then catcch adn async await 
 
+// fetch("https://randomuser.me/api/")
+// .then(rawdata=> { //function (rawdata) { // can use it
+//     console.log(rawdata); // raw data gives response , in body their is data as redeable strream can not be readead so use json
+// })
+// .catch((err)=> {
+//     console.log(err);
+// });
+
+// ok true and status 200 in response means everything good
+// data in body as redeable stream so con't read it 
+// raw data hidden coomputed , can't be readed
+
 fetch("https://randomuser.me/api/")
-.then(rawdata=> { //function (rawdata) { // can use it
-    console.log(rawdata);
+.then((rawdata)=> { 
+    return rawdata.json();
+})
+.then((data)=>{
+    // console.log(data);
+    // console.log(data.results);
+    // console.log(data.results[0].name);
+    console.log(data.results[0].name.first);
 })
 .catch((err)=> {
     console.log(err);
 });
-
-// ok true and status 200 in response means everything good
