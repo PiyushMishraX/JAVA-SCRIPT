5a3e0f5f1860a07928a532a5ae755d5f92b80e20
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-20 22:19:17 +0530

    debouncin andf throttling

---
+++ Performance optimization/script.js
@@ -0,0 +1,10 @@
+// performance optimization
+
+// debouncing and throttling
+
+// jab koi specific event occur ho to jo  kisi specific interval ke baad chale wo de bouncing and jab koi code har kuch der mai chale to wo throttling - agar ek specific evetn chal raha hai tab tak har kuch der mai
+
+
+// deboucing - koi action kar rahe and aap ye nahi chahate har action pe kuchh h , jab bhi mere actons ke bech mein koi specific 
+
+
